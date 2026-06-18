1e69822a28ff8ac721981568ed9277af659c7f8e
София <sofiapasko@yandex.ru>
2026-06-19 01:51:09 +0300

    fix API production URLs

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://analysis-system-tonus-backend.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
